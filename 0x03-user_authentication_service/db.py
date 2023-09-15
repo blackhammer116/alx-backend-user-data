45864710bc2cd4dab9e65998c34a0dabe5ddffa7
Nebiyu <nebiyuhiyabu@gmail.com>
2023-09-15 07:31:28 +0000

    add users file update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email="", hashed_pw="") -> User:
+    def add_user(self, email: str, hashed_pw: str) -> User:
         """
         Class method that adds a new user to
         the database
